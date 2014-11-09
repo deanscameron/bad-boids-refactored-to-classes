6267f27210b5e8e02a49a52e996e206ae3565be7
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2014-11-09 19:21:10 +0000

    Removed repeated code with distance_check function

--- boids.py
+++ boids.py
@@ -42,7 +42,11 @@ def distance(position_1, position_2):
 
 def update_velocity(position_1, position_2, scale_factor):
     velocity_step = (position_2 - position_1)*scale_factor
-    return velocity_step	
+    return velocity_step
+
+def distance_check(x_position_1, x_position_2, y_position_1, y_position_2, range)
+    if distance((x_position_2 - x_position_1), (y_position_2 - y_position_1)) < range
+	    return True
 	
 def update_boids(boids):
 	boids_x_positions,boids_y_positions,boids_x_velocities,boids_y_velocities = boids
@@ -59,14 +63,14 @@ def update_boids(boids):
 	# Fly away from nearby boids
 	for i in range(number_boids):
 		for j in range(number_boids):
-			if distance((boids_x_positions[j]-boids_x_positions[i]), (boids_y_positions[j]-boids_y_positions[i])) < nearby_distance:
+			if distance_check(boids_x_positions[i], boids_x_positions[j], boids_y_positions[i], boids_y_positions[j], nearby_distance):
 				boids_x_velocities[i] += update_velocity(boids_x_positions[j], boids_x_positions[i], 1)
 				boids_y_velocities[i] += update_velocity(boids_y_positions[j], boids_y_positions[i], 1) 
 	
 	# Try to match speed with nearby boids
 	for i in range(number_boids):
 		for j in range(number_boids):
-			if distance((boids_x_positions[j]-boids_x_positions[i]), (boids_y_positions[j]-boids_y_positions[i])) < match_speed_distance:
+			if distance_check(boids_x_positions[i], boids_x_positions[j], boids_y_positions[i], boids_y_positions[j], match_speed_distance):
 				boids_x_velocities[i] += update_velocity(boids_x_velocities[i], boids_x_velocities[j], (velocity_match_scale_factor/number_boids))
 				boids_y_velocities[i] += update_velocity(boids_y_velocities[i], boids_y_velocities[j], (velocity_match_scale_factor/number_boids))
 	
