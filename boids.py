9b9f07e42e15d37229bb5ad23278443290c5d6b0
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2014-11-09 19:12:40 +0000

    Removed repeated code with a update_velocity function

--- boids.py
+++ boids.py
@@ -37,33 +37,39 @@ boid_start_y_velocities=[random.uniform(*start_y_vel_range) for x in range(start
 boids=(boids_start_x_pos,boids_start_y_pos,boid_start_x_velocities,boid_start_y_velocities)
 
 
-def distance(variable_1, variable_2):
-    return hypot(variable_1, variable_2)
+def distance(position_1, position_2):
+    return hypot(position_1, position_2)
 
-	
+def update_velocity(position_1, position_2, scale_factor):
+    velocity_step = (position_2 - position_1)*scale_factor
+    return velocity_step	
 	
 def update_boids(boids):
 	boids_x_positions,boids_y_positions,boids_x_velocities,boids_y_velocities = boids
 	number_boids = len(boids_x_positions)
+	
 	# Fly towards the middle
 	for i in range(number_boids):
 		for j in range(number_boids):
-			boids_x_velocities[i] += (boids_x_positions[j]-boids_x_positions[i])*velocity_scale_factor/number_boids
+			boids_x_velocities[i] += update_velocity(boids_x_positions[i], boids_x_positions[j], (velocity_scale_factor/number_boids))
 	for i in range(number_boids):
 		for j in range(number_boids):
-			boids_y_velocities[i] += (boids_y_positions[j]-boids_y_positions[i])*velocity_scale_factor/number_boids
+			boids_y_velocities[i] += update_velocity(boids_y_positions[i], boids_y_positions[j], (velocity_scale_factor/number_boids)
+	
 	# Fly away from nearby boids
 	for i in range(number_boids):
 		for j in range(number_boids):
 			if distance((boids_x_positions[j]-boids_x_positions[i]), (boids_y_positions[j]-boids_y_positions[i])) < nearby_distance:
-				boids_x_velocities[i] += (boids_x_positions[i]-boids_x_positions[j])
-				boids_y_velocities[i] += (boids_y_positions[i]-boids_y_positions[j])
+				boids_x_velocities[i] += update_velocity(boids_x_positions[j], boids_x_positions[i], 1)
+				boids_y_velocities[i] += update_velocity(boids_y_positions[j], boids_y_positions[i], 1) 
+	
 	# Try to match speed with nearby boids
 	for i in range(number_boids):
 		for j in range(number_boids):
 			if distance((boids_x_positions[j]-boids_x_positions[i]), (boids_y_positions[j]-boids_y_positions[i])) < match_speed_distance:
-				boids_x_velocities[i] += (boids_x_velocities[j]-boids_x_velocities[i])*velocity_match_scale_factor/number_boids
-				boids_y_velocities[i] += (boids_y_velocities[j]-boids_y_velocities[i])*velocity_match_scale_factor/number_boids
+				boids_x_velocities[i] += update_velocity(boids_x_velocities[i], boids_x_velocities[j], (velocity_match_scale_factor/number_boids))
+				boids_y_velocities[i] += update_velocity(boids_y_velocities[i], boids_y_velocities[j], (velocity_match_scale_factor/number_boids))
+	
 	# Move according to velocities
 	for i in range(len(boids_x_positions)):
 		boids_x_positions[i] += boids_x_velocities[i]
