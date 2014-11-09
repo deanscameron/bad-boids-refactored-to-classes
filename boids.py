8792747644001b22f58414d7a522c77fd91cee67
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2014-11-09 15:12:16 +0000

    Added distance function to replace repeated code

--- boids.py
+++ boids.py
@@ -1,5 +1,5 @@
 """
-A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
+Implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
 for use as an exercise on refactoring.
 """
 
@@ -8,9 +8,8 @@ matplotlib.use('TkAgg')
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
+from math import hypot
 
-
-# Deliberately terrible code for teaching purposes
 
 start_x_pos_range = [-450, 50.0]
 start_y_pos_range = [300.0, 600.0]
@@ -18,11 +17,11 @@ start_x_vel_range = [0, 10.0]
 start_y_vel_range = [-20.0, 20.0]
 number_boids = 50 
 
-animation_frame_num = 50
+animation_frame_number = 50
 animation_time_interval = 50
 
-nearby_distance = 100
-match_speed_distance = 10000
+nearby_distance = 10
+match_speed_distance = 100
 
 velocity_scale_factor = 0.01
 velocity_match_scale_factor = 0.125
@@ -36,6 +35,11 @@ boid_start_x_velocities=[random.uniform(*start_x_vel_range) for x in range(numbe
 boid_start_y_velocities=[random.uniform(*start_y_vel_range) for x in range(number_boids)]
 boids=(boids_start_x_pos,boids_start_y_pos,boid_start_x_velocities,boid_start_y_velocities)
 
+
+def distance(variable_1, variable_2):
+    return hypot(variable_1, variable_2)
+	
+	
 def update_boids(boids):
 	boids_x_positions,boids_y_positions,boids_x_velocities,boids_y_velocities = boids
 	# Fly towards the middle
@@ -48,13 +52,13 @@ def update_boids(boids):
 	# Fly away from nearby boids
 	for i in range(len(boids_x_positions)):
 		for j in range(len(boids_x_positions)):
-			if (boids_x_positions[j]-boids_x_positions[i])**2 + (boids_y_positions[j]-boids_y_positions[i])**2 < nearby_distance:
+			if distance((boids_x_positions[j]-boids_x_positions[i]), (boids_y_positions[j]-boids_y_positions[i])) < nearby_distance:
 				boids_x_velocities[i]=boids_x_velocities[i]+(boids_x_positions[i]-boids_x_positions[j])
 				boids_y_velocities[i]=boids_y_velocities[i]+(boids_y_positions[i]-boids_y_positions[j])
 	# Try to match speed with nearby boids
 	for i in range(len(boids_x_positions)):
 		for j in range(len(boids_x_positions)):
-			if (boids_x_positions[j]-boids_x_positions[i])**2 + (boids_y_positions[j]-boids_y_positions[i])**2 < match_speed_distance:
+			if distance((boids_x_positions[j]-boids_x_positions[i]), (boids_y_positions[j]-boids_y_positions[i])) < match_speed_distance:
 				boids_x_velocities[i]=boids_x_velocities[i]+(boids_x_velocities[j]-boids_x_velocities[i])*velocity_match_scale_factor/len(boids_x_positions)
 				boids_y_velocities[i]=boids_y_velocities[i]+(boids_y_velocities[j]-boids_y_velocities[i])*velocity_match_scale_factor/len(boids_x_positions)
 	# Move according to velocities
@@ -73,7 +77,7 @@ def animate(frame):
 
 
 anim = animation.FuncAnimation(figure, animate,
-                               frames = animation_frame_num, 
+                               frames = animation_frame_number, 
 							   interval = animation_time_interval)
 
 if __name__ == "__main__":
