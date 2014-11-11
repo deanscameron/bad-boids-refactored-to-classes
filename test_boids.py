d655451fd173f19b89760e15c781a22da89753ec
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2014-11-11 11:14:30 +0000

    Added test for move boids

--- test_boids.py
+++ test_boids.py
@@ -37,6 +37,14 @@ def test_match_velocities():
         for after_value,before_value in zip(after,before): 
             assert_almost_equal(after_value,before_value,delta=0.01)
 			
+def test_move_boids():
+    regression_data=yaml.load(open(os.path.join(os.path.dirname(__file__),'move_boids.yml')))
+    boid_data=regression_data["before"]
+    move_boids(boid_data)
+    for after,before in zip(regression_data["after"],boid_data):
+        for after_value,before_value in zip(after,before): 
+            assert_almost_equal(after_value,before_value,delta=0.01)
+			
 def test_distance():
     assert_equal(distance(3,4),5)
     assert_almost_equal(distance(-1,1), 1.41, delta=0.01)
