943f3643473590280d3112ab643bac02938c7397
Cameron Deans <cameron.deans.14@ucl.ac.uk>
2014-11-11 16:19:30 +0000

    Completed refactoring to class structure

--- boids.py
+++ boids.py
@@ -10,17 +10,15 @@ from matplotlib import animation
 import random
 import config
 import yaml
-from boids_functions import distance, update_velocity, distance_check
-
+from boids_functions import update_vel, dist_check
 config=yaml.load(open("config.yml"))
-
 
 class Boid(object):
     def __init__(self, x_position, y_position, x_velocity, y_velocity):
-        self.x_position = x_position
-        self.y_position = y_position
-        self.x_velocity = x_velocity
-        self.y_velocity = y_velocity
+        self.x_pos = x_position
+        self.y_pos = y_position
+        self.x_vel = x_velocity
+        self.y_vel = y_velocity
         
 class Boids(object):
     def __init__(self, num_boids=config['number_boids'], 
@@ -29,54 +27,62 @@ class Boids(object):
 					start_x_vel_range = config['start_x_vel_range'],
 					start_y_vel_range = config['start_y_vel_range'],
 					velocity_scale_factor = config['velocity_scale_factor'],
-					velocity_match_scale_factor = config['velocity_match_scale_factor'],
+					velocity_match_factor = config['velocity_match_factor'],
 					nearby_distance = config['nearby_distance'],
 					match_speed_distance = config['match_speed_distance']):
-	    self.num_boids = num_boids
-	    self.start_x_pos_range = start_x_pos_range
-	    self.start_y_pos_range = start_y_pos_range
-	    self.start_x_vel_range = start_x_vel_range
-	    self.start_y_vel_range = start_y_vel_range
-	    self.velocity_scale_factor = velocity_scale_factor
-	    self.velocity_match_scale_factor = velocity_match_scale_factor
-	    self.nearby_distance = nearby_distance
-	    self.match_speed_distance = match_speed_distance
+	    self.num = num_boids
+	    self.x_range = start_x_pos_range
+	    self.y_range = start_y_pos_range
+	    self.x_vel_range = start_x_vel_range
+	    self.y_vel_range = start_y_vel_range
+	    self.vel_fact = velocity_scale_factor
+	    self.match_fact = velocity_match_factor
+	    self.near_dist = nearby_distance
+	    self.match_dist = match_speed_distance
 	    self.random_boids()
 
     def random_boids(self):
-        self.boids = [Boid(random.uniform(*self.start_x_pos_range),
-                random.uniform(*self.start_y_pos_range),
-                random.uniform(*self.start_x_vel_range),
-                random.uniform(*self.start_y_vel_range)) for x in range(self.num_boids)]
+        self.boids = [Boid(random.uniform(*self.x_range),
+                random.uniform(*self.y_range),
+                random.uniform(*self.x_vel_range),
+                random.uniform(*self.y_vel_range)) for x in range(self.num)]
 		
     def fly_towards_middle(self):
-        # Update the velocities of boids such that they fly towards the centre of the flock
+        # Update the velocities of boids such that they fly towards the centre
         for boid_1 in self.boids:
             for boid_2 in self.boids:
-                boid_1.x_velocity += update_velocity(boid_1.x_position, boid_2.x_position, (self.velocity_scale_factor/self.num_boids))
-                boid_1.y_velocity += update_velocity(boid_1.y_position, boid_2.y_position, (self.velocity_scale_factor/self.num_boids))
+                boid_1.x_vel += update_vel(boid_1.x_pos, boid_2.x_pos, 
+												(self.vel_fact/self.num))
+                boid_1.y_vel += update_vel(boid_1.y_pos, boid_2.y_pos, 
+												(self.vel_fact/self.num))
 
     def avoid_nearby_boids(self):   
-        # Update the velocities of boids such that they fly away from nearby birds in the flock
+        # Update the velocities of boids such that they fly away from nearby
+		# birds in the flock
         for boid_1 in self.boids:
             for boid_2 in self.boids:
-                if distance_check(boid_1.x_position, boid_2.x_position, boid_1.y_position, boid_2.y_position, self.nearby_distance):
-                    boid_1.x_velocity += update_velocity(boid_2.x_position, boid_1.x_position, 1)
-                    boid_1.y_velocity += update_velocity(boid_2.y_position, boid_1.y_position, 1)
+                if dist_check(boid_1.x_pos, boid_2.x_pos, boid_1.y_pos, 
+												boid_2.y_pos, self.near_dist):
+                    boid_1.x_vel += update_vel(boid_2.x_pos, boid_1.x_pos, 1)
+                    boid_1.y_vel += update_vel(boid_2.y_pos, boid_1.y_pos, 1)
 
     def match_velocities(self):
-        # Update the velocities of boids such that they fly at the same speed as the rest of the flock
+        # Update the velocities of boids such that they fly at the same 
+		#speed as the rest of the flock
         for boid_1 in self.boids:
             for boid_2 in self.boids:
-                if distance_check(boid_1.x_position, boid_2.x_position, boid_1.y_position, boid_2.y_position, self.match_speed_distance):
-                    boid_1.x_velocity += update_velocity(boid_1.x_velocity, boid_2.x_velocity, (self.velocity_match_scale_factor/self.num_boids))
-                    boid_1.y_velocity += update_velocity(boid_1.y_velocity, boid_2.y_velocity, (self.velocity_match_scale_factor/self.num_boids))  
+                if dist_check(boid_1.x_pos, boid_2.x_pos, boid_1.y_pos, 
+											boid_2.y_pos, self.match_dist):
+                    boid_1.x_vel += update_vel(boid_1.x_vel, boid_2.x_vel, 
+											(self.match_fact/self.num))
+                    boid_1.y_vel += update_vel(boid_1.y_vel, boid_2.y_vel,
+											(self.match_fact/self.num))  
 
     def move_boids(self):
         # Boids fly according to their velocities
         for boid_1 in self.boids:
-            boid_1.x_position += boid_1.x_velocity
-            boid_1.y_position += boid_1.y_velocity
+            boid_1.x_pos += boid_1.x_vel
+            boid_1.y_pos += boid_1.y_vel
 
     def update_boids(self):
         self.fly_towards_middle()
@@ -87,20 +93,20 @@ class Boids(object):
     def boids_x_vector(self):
         v = []
         for boid_1 in self.boids:
-            v.append(boid_1.x_position)
+            v.append(boid_1.x_pos)
         return v
             
     def boids_y_vector(self):
         v = []
         for boid_1 in self.boids:
-            v.append(boid_1.y_position)
+            v.append(boid_1.y_pos)
         return v
     
     
 boids = Boids()
 
 figure = plt.figure()
-axes = plt.axes(xlim = config['plot_x_limits'], ylim = config['plot_y_limits'])
+axes = plt.axes(xlim = config['x_limits'], ylim = config['y_limits'])
 scatter = axes.scatter(boids.boids_x_vector(), boids.boids_y_vector())
 
 def animate(frame):
--- boids_functions.py
+++ boids_functions.py
@@ -5,10 +5,10 @@ from math import hypot
 def distance(position_1, position_2):
     return hypot(position_1, position_2)
 
-def update_velocity(position_1, position_2, scale_factor):
+def update_vel(position_1, position_2, scale_factor):
     velocity_step = (position_2 - position_1)*scale_factor
     return velocity_step
 
-def distance_check(x_position_1, x_position_2, y_position_1, y_position_2, range):
+def dist_check(x_position_1, x_position_2, y_position_1, y_position_2, range):
     if distance((x_position_2 - x_position_1), (y_position_2 - y_position_1)) < range:
 	    return True
